44f8ad7d8f3b6d1356f44bda69c12fe44364b76e
KABRAN REBECCA <prunekabran@gmail.com>
2026-05-11 19:27:11 +0000

    Version finale : ajout et correction des classes Piece, Position, Rook, Bishop, Queen et Main avec constructeurs et tests fonctionnels

--- src/Piece.java
+++ src/Piece.java
@@ -2,14 +2,21 @@ class Piece {
     int row;
     int column;
 
+    // Constructeur par défaut
+    Piece() {
+        this.row = 0;
+        this.column = 0;
+    }
+
+    // Constructeur avec paramètres
     Piece(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
     boolean isValidMove(Position newPosition) {
-        // Par défaut : aucun mouvement valid
-        return false;
+        return false; // par défaut
     }
 }
+
 
--- src/Queen.java
+++ src/Queen.java
@@ -1,17 +1,18 @@
 class Queen extends Piece {
+    Queen(int row, int column) {
+        super(row, column);
+    }
+
     @Override
     boolean isValidMove(Position newPosition) {
-        // Déplacement comme une tour
         if (newPosition.column == this.column || newPosition.row == this.row) {
-            return true;
-        }
-        // Déplacement comme un fou
-        else if (Math.abs(newPosition.column - this.column) ==
+            return true; // comme une tour
+        } else if (Math.abs(newPosition.column - this.column) ==
                 Math.abs(newPosition.row - this.row)) {
-            return true;
-        }
-        else {
+            return true; // comme un fou
+        } else {
             return false;
         }
     }
 }
+
